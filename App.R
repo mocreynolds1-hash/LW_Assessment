c789cb79832bfbfd26035a6eb7c8166e84b9100d
mocreynolds1-hash <mocreynolds1@gmail.com>
2025-12-01 12:06:28 +0000

    Update App.R

--- App.R
+++ App.R
@@ -22,17 +22,17 @@ library(leafem)
 options(shiny.maxRequestSize = 1000 * 1024^2)
 
 # Run global script containing all your relevant data ----
-source("/Users/milliereynolds/Documents/Masters/GIS/Practicals/Global.R")
+source("Global.R")
 
 # Define UI for visualisation ----
-source("/Users/milliereynolds/Documents/Masters/GIS/Practicals/UI.R")
+source("UI.R")
 ui <-  navbarPage("Instream Large Wood on the River Torridge", id = 'nav', 
              tabPanel("Map", div(class="outer", 
                                  leafletOutput("map", height = "calc(100vh - 70px)") ) ) )
 
 # Define the server that performs all necessary operations ----
 server <- function(input, output, session){
-  source("/Users/milliereynolds/Documents/Masters/GIS/Practicals/Server_Function.R", local = TRUE)
+  source("Server_Function.R", local = TRUE)
 }
 
 shinyApp(ui, server)
