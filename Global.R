3ddedb6fb45abb25f97e4d4c4e6c055f4ad36acd
mocreynolds1-hash <mocreynolds1@gmail.com>
2025-12-01 12:07:42 +0000

    Update Global.R

--- Global.R
+++ Global.R
@@ -1,9 +1,9 @@
 # G1 Load large wood, river, and bridge data ----
-lw_points <- st_read("/Users/milliereynolds/Documents/Masters/GIS/Practicals/cleaned_lw_shapefile.shp")
-river <- st_read("/Users/milliereynolds/Documents/Masters/GIS/Practicals/Part A/RiverTorridge.shp")
-bridges <- st_read("/Users/milliereynolds/Documents/Masters/GIS/Practicals/relevant bridges.shp")
+lw_points <- st_read("cleaned_lw_shapefile.shp")
+river <- st_read("RiverTorridge.shp")
+bridges <- st_read("relevant bridges.shp")
 bridges <- bridges[!st_is_empty(bridges$geometry), ]
-bridges_snapped2 <- read.csv("/Users/milliereynolds/Bridges_snapped2")
+bridges_snapped2 <- read.csv("Bridges_snapped2")
 bridges <- left_join(bridges, bridges_snapped2, by = "BRIDGE.NAM")
 
 #Convert vectors to CRS 4326
@@ -12,7 +12,7 @@ river <- st_transform(river, crs = 4326)
 bridges <- st_transform(bridges, crs = 4326)
 
 # Add Wood catchers
-wood_catchers <- st_read("/Users/milliereynolds/Documents/Masters/GIS/Practicals/wood_catcher_R2.shp")
+wood_catchers <- st_read("wood_catcher_R2.shp")
 wood_catchers <- st_transform(wood_catchers, crs = 4326)
 relabel_map <- c(
   "1" = 2,
@@ -26,14 +26,14 @@ relabel_map <- c(
 wood_catchers$id <- relabel_map[as.character(wood_catchers$id)]
 
 # Perform clustering
-clusters2 <- st_read("/Users/milliereynolds/Documents/Masters/GIS/Practicals/Clusters for Practical 6.shp")
+clusters2 <- st_read("Clusters for Practical 6.shp")
 clusters2 <- st_transform(clusters2, crs = 4326)
 # Dynamically generate colours based on number of unique clusters
 num_clusters <- length(unique(clusters2$CLUSTER_ID))
 pal_clusters <- colorFactor(palette = colorRampPalette(brewer.pal(12, "Paired"))(num_clusters), 
                             domain = clusters2$CLUSTER_ID)
 
-heatmap <- rast("/Users/milliereynolds/Documents/Masters/GIS/Practicals/Heatmap.tif")
+heatmap <- rast("Heatmap.tif")
 heatmap <- project(heatmap, crs(river))
 
 pal_heatmap <- colorNumeric(palette = "plasma", domain = na.omit(values(heatmap)), na.color = "transparent")
@@ -45,7 +45,7 @@ pal_heatmap <- colorNumeric(palette = "plasma", domain = na.omit(values(heatmap)
 
 #pal_slope1 <- colorNumeric(palette = "rocket", domain = na.omit(values(slope1)), na.color = "transparent")
 
-aspect1 <- rast("/Users/milliereynolds/aspect.tif")
+aspect1 <- rast("aspect.tif")
 aspect1 <- project(aspect1, crs(river))
 aspect1 <- aggregate(aspect1, fact = 3)
 pal_aspect1 <- colorNumeric(palette = "viridis", domain = na.omit(values(aspect1)), na.color = "transparent")
